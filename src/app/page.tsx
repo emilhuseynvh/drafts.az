a3a24133cd90527f9b1acc9b0fc09386edaf1765
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-01-26 18:43:42 +0400

    adding responive to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Services from '@/components/services';
+import Button from '@/components/ui/Button';
 import { Metadata } from 'next';
 import React from 'react'
 
@@ -40,7 +41,28 @@ export const metadata: Metadata = {
 
 const page = () => {
   return (
-    <div>
+    <div className='wrapper'>
+      <div className='flex justify-between items-start my-[60px] flex-col xs:flex-row'>
+        <div className="flex items-center space-x-3 pb-7 xs:py-0">
+          <div className="w-2 h-2 rounded-full bg-[#F7F7F1]"></div>
+          <span className="text-lg text-[#F7F7F1]">About</span>
+        </div>
+        <div>
+          <div className='max-w-3xs md:max-w-lg lg:max-w-[740px] w-full'>
+            <p className='hidden md:block text-[#F7F7F1] mb-7'>Drafts is a platform that provides high-quality design and software development services to its clients. We work tirelessly to develop your projects and offer solutions tailored to your needs. At Drafts, we prioritize long-term collaborations and are committed to delivering innovative services that help our clients achieve their goals. Our mission is to ensure the success of your business by providing top-notch services.</p>
+            <p className='block md:hidden text-[#F7F7F1] mb-7'>Drafts offers high-quality design and software development services, tailored to your needs. We focus on long-term partnerships, delivering innovative solutions to help your business succeed.</p>
+            <Button width='132px'>
+              <span className='z-10 text-sm sm:text-base group-hover:text-white hidden md:block'>About us</span>
+              <span className='z-10 text-sm sm:text-base group-hover:text-white block md:hidden'>More</span>
+              <svg className='z-10' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path className='group-hover:stroke-white' d="M16.9508 7.0498L7.05078 16.9498" stroke="#151515" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path className='group-hover:stroke-white' d="M10 7L16.95 7.049L17 14" stroke="#111213" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+
+            </Button>
+          </div>
+        </div>
+      </div>
       <Services />
     </div>
   )
