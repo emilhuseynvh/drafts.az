64aa9567c94b41975c0b3f07ce7346eec0f1948c
emilhuseynvh <emilhuseynvh@gmail.com>
2025-01-27 16:03:01 +0400

    our work section updated

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { EffectFade, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -10,6 +10,8 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 const OurWork: React.FC = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div className='bg-white px-3 py-5 rounded-2xl relative'>
       <div className="flex items-center space-x-2 mb-7 xs:mb-0 absolute">
@@ -20,27 +22,38 @@ const OurWork: React.FC = () => {
         spaceBetween={30}
         effect={'fade'}
         navigation={true}
-
         pagination={{
           clickable: true,
         }}
         modules={[EffectFade, Navigation]}
         className="mySwiper"
+        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
       >
         <SwiperSlide>
-          <p className='text-end text-base'>VovaFM Landing Page</p>
-          <video className='mt-3.5 rounded-2xl h-70 object-cover' src="video/VovaFM.mp4" autoPlay muted loop></video>
-          <p className='mt-3 mb-20 text-[17px] italic'>We provide design and programming services tailored to every individual need.</p>
+          {activeIndex === 0 && (
+            <>
+              <p className='text-end text-base'>VovaFM Landing Page</p>
+              <video className='mt-3.5 rounded-2xl h-70 object-cover' src="video/VovaFM.mp4" autoPlay muted loop></video>
+            </>
+          )}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt='zirt' />
+          {activeIndex === 1 && (
+            <>
+              <p className='text-end text-base'>RideX Mobile App</p>
+              <video className='mt-3.5 rounded-2xl h-70 object-cover' src="video/RideX.mp4" autoPlay muted loop></video>
+            </>
+          )}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt='zirt' />
+          {activeIndex === 2 && (
+            <>
+              <p className='text-end text-base'>Techchore Branding Design</p>
+              <video className='mt-3.5 rounded-2xl h-70 object-cover' src="video/Techocre.mp4" autoPlay muted loop></video>
+            </>
+          )}
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt='zirt' />
-        </SwiperSlide>
+      <p className='mt-3 mb-20 text-[17px] italic'>We provide design and programming services tailored to every individual need.</p>
       </Swiper>
     </div>
   );
