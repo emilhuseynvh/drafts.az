81fb6e983d112980f78f9755ec8aa4d26b7af589
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-03-04 15:24:44 +0400

    contact page added and add some changes

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -5,6 +5,7 @@ import React, { Dispatch, SetStateAction } from 'react';
 import language from './../../public/icons/lang.svg';
 import { usePathname } from 'next/navigation';
 import { useRouter } from '@/i18n/routing';
+import Link from 'next/link';
 
 interface HamgurgerProps {
     flag: boolean
@@ -16,21 +17,20 @@ const HamburgerMenu: React.FC<HamgurgerProps> = ({ flag, setFlag }) => {
     const router = useRouter();
     const pathname = usePathname();
     const currentLocale = useLocale();
-    
+
     const changeLanguage = (newLocale: string) => {
         const pathWithoutLocale = pathname.replace(`/${currentLocale}`, '');
         const newPath = pathWithoutLocale || '/';
         router.replace(newPath, { locale: newLocale });
     };
-    
+
     return (
         <div className={`bg-primary-bg ${flag ? 'right-0' : '-right-full'} duration-500 w-full top-17 h-screen fixed z-50 md:hidden flex justify-end px-4 py-7`}>
             <div>
                 <ul className='text-end text-primary-white text-4xl'>
-                    <li className='my-3.5 font-light'>{t('header.list.home_page')}</li>
-                    <li className='my-3.5 font-light'>{t('header.list.services')}</li>
-                    <li className='my-3.5 font-light'>{t('header.list.about')}</li>
-                    <li className='my-3.5 font-light'>{t('header.list.contact')}</li>
+                    <li className='my-3.5 font-light'><Link href={`/${currentLocale}`}>{t('header.list.home_page')}</Link></li>
+                    <li className='my-3.5 font-light'><Link href={`/${currentLocale}/about`}>{t('header.list.about')}</Link></li>
+                    <li className='my-3.5 font-light'><Link href={`/${currentLocale}/contact`}>{t('header.list.contact')}</Link></li>
                 </ul>
                 <div>
                     <div className='flex gap-3 mt-10 py-5 border-t border-[#AAA]'>
@@ -38,20 +38,20 @@ const HamburgerMenu: React.FC<HamgurgerProps> = ({ flag, setFlag }) => {
                         <p className='text-primary-white text-sm'>Dil Seçimi</p>
                     </div>
                     <div className='flex gap-2.5'>
-                        <button 
-                            onClick={() => changeLanguage('az')} 
+                        <button
+                            onClick={() => changeLanguage('az')}
                             className={`${currentLocale === 'az' ? 'border-[#DB3D3E] text-[#DB3D3E]' : 'border-[#DDD]'} py-1.5 px-3.5 border-2 text-sm font-medium rounded-[10px] bg-white`}
                         >
                             Aze
                         </button>
-                        <button 
-                            onClick={() => changeLanguage('en')} 
+                        <button
+                            onClick={() => changeLanguage('en')}
                             className={`${currentLocale === 'en' ? 'border-[#DB3D3E] text-[#DB3D3E]' : 'border-[#DDD]'} py-1.5 px-3.5 border-2 text-sm font-medium rounded-[10px] bg-white`}
                         >
                             Eng
                         </button>
-                        <button 
-                            onClick={() => changeLanguage('ru')} 
+                        <button
+                            onClick={() => changeLanguage('ru')}
                             className={`${currentLocale === 'ru' ? 'border-[#DB3D3E] text-[#DB3D3E]' : 'border-[#DDD]'} py-1.5 px-3.5 border-2 text-sm font-medium rounded-[10px] bg-white`}
                         >
                             Rus
